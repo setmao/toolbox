e489710484c4bb941b19b53dc1f4a57ff1343b37
SetMao <set.mao.tw@gmail.com>
2025-04-25 22:19:30 +0800

    [feat] add DefaultLayout component and update routing structure

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import JSONFormatter from '@/pages/JSONFormatter';
+import DefaultLayout from '@/layouts/DefaultLayout';
 
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<JSONFormatter />} />
+          <Route path="/" element={<DefaultLayout />}>
+            <Route index element={<Navigate to="/json-formatter" replace />} />
+            <Route path="/json-formatter" element={<JSONFormatter />} />
+          </Route>
       </Routes>
     </BrowserRouter>
   );
